be4118a6b6195eb2d3754452a726dc89e8c561d8
hansgra <hansgra@gmail.com>
2013-09-02 22:43:37 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -9,9 +9,9 @@ module.exports = {
             name: 'MEAN - A Modern Stack - Development'
         },
         facebook: {
-            clientID: "APP_ID",
-            clientSecret: "APP_SECRET",
-            callbackURL: "http://localhost:3000/auth/facebook/callback"
+            clientID: "463519200411932",
+            clientSecret: "1481f6153834938b3dccdfaeae884dea",
+            callbackURL: "http:/giftr.cloudapp.net:3000/auth/facebook/callback"
         },
         twitter: {
             clientID: "CONSUMER_KEY",
