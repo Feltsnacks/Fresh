3dc98ff933b7ceedf1faab59f94f1eae902b4fd7
samunit <alexander.wehlin@gmail.com>
2013-12-05 17:43:47 +0100

    addad

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -27,6 +27,34 @@ app.controller("getListCtrl", function ($scope, $http) {
   }
 );
 
+app.factory('srvAuth', function(){
+
+watchLoginChange = function() {
+
+  var _self = this;
+  FB.Event.subscribe('auth.authResponseChange', function(response) {
+
+    if (response.status === 'connected') {
+      _self.getUserInfo();
+      /*
+       This is also the point where you should create a 
+       session for the current user.
+       For this purpose you can use the data inside the 
+       response.authResponse object.
+      */
+
+    } 
+    else 
+    {
+      /*
+       The user is not logged to the app, or into Facebook:
+       destroy the session on the server.
+      */
+    }
+  });
+  }
+});
+
 app.run(['$rootScope', '$window', 'srvAuth', 
   function($rootScope, $window, sAuth) {
 
@@ -99,54 +127,3 @@ app.run(['$rootScope', '$window', 'srvAuth',
   }(document));
 
 }]);
-
-watchLoginChange = function() {
-
-  var _self = this;
-
-  FB.Event.subscribe('auth.authResponseChange', function(response) {
-
-    if (response.status === 'connected') {
-      
-      /* 
-       The user is already logged, 
-       is possible retrieve his personal info
-      */
-      _self.getUserInfo();
-
-      /*
-       This is also the point where you should create a 
-       session for the current user.
-       For this purpose you can use the data inside the 
-       response.authResponse object.
-      */
-
-    } 
-    else {
-
-      /*
-       The user is not logged to the app, or into Facebook:
-       destroy the session on the server.
-      */
-       
-    }
-
-  });
-
-}
-
-getUserInfo = function() {
-
-  var _self = this;
-
-  FB.api('/me', function(response) {
-
-    $rootScope.$apply(function() { 
-
-      $rootScope.user = _self.user = response; 
-
-    });
-
-  });
-
-}
