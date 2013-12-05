97a13b3d6fce40a3d58ba2b29efa8c611bddb38d
samunit <alexander.wehlin@gmail.com>
2013-12-05 18:37:01 +0100

    addad

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,54 @@
-var app = angular.module('Site', ['srvAuth']);
+var app = angular.module('Site', []);
+
+app.factory('srvAuth', ['$rootScope', function() {
+  return {
+    watchAuthenticationStatusChange: function() {
+      var _self = this;
+      FB.Event.subscribe('auth.authResponseChange', function(response) {
+
+        if (response.status === 'connected') {
+          _self.getUserInfo();
+          /*
+           This is also the point where you should create a 
+           session for the current user.
+           For this purpose you can use the data inside the 
+           response.authResponse object.
+          */
+
+        } 
+        else 
+        {
+          /*
+           The user is not logged to the app, or into Facebook:
+           destroy the session on the server.
+          */
+        }
+      });
+      },
+
+    getUserInfo: function() {
+
+      var _self = this;
+
+      FB.api('/me', function(response) {
+        $rootScope.$apply(function() { 
+          $rootScope.user = _self.user = response; 
+        });
+      });
+    },
+
+    logout: function() {
+
+      var _self = this;
+
+      FB.logout(function(response) {
+        $rootScope.$apply(function() { 
+          $rootScope.user = _self.user = {}; 
+        }); 
+      });
+    }
+  }
+}]);
 
 app.config(function($routeProvider){
   $routeProvider
