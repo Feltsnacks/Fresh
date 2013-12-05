a314cf67087c59a2f2805697536020dc31012e9e
samunit <alexander.wehlin@gmail.com>
2013-12-05 18:49:34 +0100

    daada

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -129,24 +129,6 @@ app.run(['$rootScope', '$window', 'srvAuth',
 
   // Are you familiar to IIFE ( http://bit.ly/iifewdb ) ?
 
-  (function(d){
-    // load the Facebook javascript SDK
-
-    var js, 
-    id = 'facebook-jssdk', 
-    ref = d.getElementsByTagName('script')[0];
-
-    if (d.getElementById(id)) {
-      return;
-    }
-
-    js = d.createElement('script'); 
-    js.id = id; 
-    js.async = true;
-    js.src = "//connect.facebook.net/en_US/all.js";
-
-    ref.parentNode.insertBefore(js, ref);
-
-  }(document));
+  
 
 }]);
