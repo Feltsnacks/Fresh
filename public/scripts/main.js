02ae75a5f965f94bb86f0dfafea21da7a48aea87
samunit <alexander.wehlin@gmail.com>
2013-12-05 18:57:48 +0100

    dadad

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,7 +2,7 @@ var app = angular.module('Site', []);
 
 app.factory('srvAuth', ['$rootScope', function($rootScope) {
   return {
-    watchAuthenticationStatusChange: function() {
+    authResponseChange: function() {
       var _self = this;
       FB.Event.subscribe('auth.authResponseChange', function(response) {
 
@@ -84,42 +84,12 @@ app.run(['$rootScope', '$window', 'srvAuth',
   $rootScope.user = {};
 
   $window.fbAsyncInit = function() {
-    // Executed when the SDK is loaded
 
     FB.init({ 
-
-      /* 
-       The app id of the web app;
-       To register a new app visit Facebook App Dashboard
-       ( https://developers.facebook.com/apps/ ) 
-      */
-
       appId: '463519200411932', 
-
-      /* 
-       Adding a Channel File improves the performance 
-       of the javascript SDK, by addressing issues 
-       with cross-domain communication in certain browsers. 
-      */
-
       channelUrl: 'app/channel.html', 
-
-      /* 
-       Set if you want to check the authentication status
-       at the start up of the app 
-      */
-
       status: true, 
-
-      /* 
-       Enable cookies to allow the server to access 
-       the session 
-      */
-
       cookie: true, 
-
-      /* Parse XFBML */
-
       xfbml: true 
     });
 
@@ -127,8 +97,25 @@ app.run(['$rootScope', '$window', 'srvAuth',
 
   };
 
-  // Are you familiar to IIFE ( http://bit.ly/iifewdb ) ?
 
-  
+  (function(d){
+    // load the Facebook javascript SDK
+
+    var js, 
+    id = 'facebook-jssdk', 
+    ref = d.getElementsByTagName('script')[0];
+
+    if (d.getElementById(id)) {
+      return;
+    }
+
+    js = d.createElement('script'); 
+    js.id = id; 
+    js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+
+    ref.parentNode.insertBefore(js, ref);
+
+  }(document));
 
 }]);
