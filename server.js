9bbbc6da2ae7b92caa77343ae934303b000bdb39
hansgra <hansgra@gmail.com>
2013-12-05 15:38:59 +0100

    .......

--- server.js
+++ server.js
@@ -28,6 +28,39 @@ app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Passport security setup
+var FACEBOOK_APP_ID = '463519200411932';
+var FACEBOOK_APP_SECRET = '1481f6153834938b3dccdfaeae884dea';
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Facebook profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: 'http://giftr.cloudapp.net:' + app.get('port') + '/auth/facebook/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    user.findOrCreate(accessToken, refreshToken, profile, function(err, loggedInUser) {
+      if (err) { return done(err); }
+      done(null, loggedInUser);
+    });
+  }
+));
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
@@ -40,20 +73,6 @@ app.get('/', function(req, res){
 app.get('/api', ensureAuthenticated, function(req, res){
 	res.sendfile('./api/index.html');
 });
-
-// Passport security  
-passport.use(new FacebookStrategy({
-    clientID: '463519200411932',
-    clientSecret: '1481f6153834938b3dccdfaeae884dea',
-    callbackURL: 'http://giftr.cloudapp.net:' + app.get('port') + '/auth/facebook/callback'
-  },
-  function(accessToken, refreshToken, profile, done) {
-    user.findOrCreate(accessToken, refreshToken, profile, function(err, loggedInUser) {
-      if (err) { return done(err); }
-      done(null, loggedInUser);
-    });
-  }
-));
 
 // Redirect the user to Facebook for authentication.  When complete,
 // Facebook will redirect the user back to the application at
