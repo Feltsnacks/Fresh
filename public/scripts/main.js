33cb99527c3ea6eaf652fbafcd82966282e34bb1
samunit <alexander.wehlin@gmail.com>
2013-12-05 18:09:42 +0100

    dudu

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -27,65 +27,54 @@ app.controller("getListCtrl", function ($scope, $http) {
   }
 );
 
-app.factory('srvAuth', function(){
-return{
-watchAuthenticationStatusChange: function() {
-  var _self = this;
-  FB.Event.subscribe('auth.authResponseChange', function(response) {
+app.factory('srvAuth', function() {
+  return {
+    watchAuthenticationStatusChange: function() {
+      var _self = this;
+      FB.Event.subscribe('auth.authResponseChange', function(response) {
 
-    if (response.status === 'connected') {
-      _self.getUserInfo();
-      /*
-       This is also the point where you should create a 
-       session for the current user.
-       For this purpose you can use the data inside the 
-       response.authResponse object.
-      */
+        if (response.status === 'connected') {
+          _self.getUserInfo();
+          /*
+           This is also the point where you should create a 
+           session for the current user.
+           For this purpose you can use the data inside the 
+           response.authResponse object.
+          */
 
-    } 
-    else 
-    {
-      /*
-       The user is not logged to the app, or into Facebook:
-       destroy the session on the server.
-      */
+        } 
+        else 
+        {
+          /*
+           The user is not logged to the app, or into Facebook:
+           destroy the session on the server.
+          */
+        }
+      });
+      },
+
+    getUserInfo: function() {
+
+      var _self = this;
+
+      FB.api('/me', function(response) {
+        $rootScope.$apply(function() { 
+          $rootScope.user = _self.user = response; 
+        });
+      });
+    },
+
+    logout: function() {
+
+      var _self = this;
+
+      FB.logout(function(response) {
+        $rootScope.$apply(function() { 
+          $rootScope.user = _self.user = {}; 
+        }); 
+      });
     }
-  });
-  },
-
-getUserInfo: function() {
-
-  var _self = this;
-
-  FB.api('/me', function(response) {
-
-    $rootScope.$apply(function() { 
-
-      $rootScope.user = _self.user = response; 
-
-    });
-
-  });
-
-},
-
-logout: function() {
-
-  var _self = this;
-
-  FB.logout(function(response) {
-
-    $rootScope.$apply(function() { 
-
-      $rootScope.user = _self.user = {}; 
-
-    }); 
-
-  });
-
-}
-
-}
+  }
 });
 
 app.run(['$rootScope', '$window', 'srvAuth', 
